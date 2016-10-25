dfeb1b3831ef885e82d2c9ed74d882c220834e57
Allen Cook <alcook16@gmail.com>
2016-10-25 13:28:40 -0400

    Flip my textures! refactored CUVs to be a little easier to remember what the heck is going on

--- src/main/java/net/slimevoid/camolib/client/model/ModelBlockCamo.java
+++ src/main/java/net/slimevoid/camolib/client/model/ModelBlockCamo.java
@@ -5,12 +5,15 @@ import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ItemOverrideList;
+import net.minecraft.client.renderer.color.BlockColors;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.client.model.pipeline.IVertexProducer;
 import net.minecraftforge.common.property.IExtendedBlockState;
@@ -20,6 +23,7 @@ import net.slimevoid.camolib.core.lib.ConfigLib;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -63,76 +67,141 @@ public class ModelBlockCamo implements IBakedModel {
     }
 
     private BakedQuad CamoQuad(IExtendedBlockState origState,IBlockState camo,BakedQuad origFace,BakedQuad quad) {
-        int[] UVs = new int[12];
-
+        int[] Cs = new int[4];
+        int[] UVs = new int[8];
+        int[] targetInts = quad.getVertexData();
         //fill in Color, U, V info
         for (int i = 0; i < 4; ++i) {
-            UVs[(i * 3)] = quad.getVertexData()[(i * 7) + 3];
-            UVs[(i * 3) + 1] = quad.getVertexData()[(i * 7) + 4];
-            UVs[(i * 3) + 2] = quad.getVertexData()[(i * 7) + 5];
+            Cs[i] = targetInts[(i * 7) + 3];
+            UVs[(i * 2) ] = targetInts[(i * 7) + 4];
+            UVs[(i * 2) + 1] = targetInts[(i * 7) + 5];
         }
+        CalcColor(origState, camo, quad.hasTintIndex(), Cs);
+
+        //Finally replace color, U and V info for each face in the camo model
+        int origInts[] = origFace.getVertexData();
+        UVs = ReplaceTexture(origInts,origFace.getFace(), UVs);
+
+        //this returns an array in the following format
+        //X,Y,Z,Color,U,V,?Light? for each point
+        int finalInts[] = {
+                origInts[0], origInts[1], origInts[2], Cs[0], UVs[0], UVs[1],origInts[6],
+                origInts[7], origInts[8], origInts[9], Cs[1], UVs[2], UVs[3],origInts[13],
+                origInts[14], origInts[15], origInts[16], Cs[2], UVs[4], UVs[5],origInts[20],
+                origInts[21], origInts[22], origInts[23], Cs[3], UVs[6], UVs[7],origInts[27]};
+        return new BakedQuad(finalInts, origFace.getTintIndex(), origFace.getFace(),quad.getSprite(),true, DefaultVertexFormats.ITEM);
+    }
+
+    private int[] CalcColor(IExtendedBlockState origState, IBlockState camo, boolean hasTint, int[] Cs) {
+        int renderLayer = MinecraftForgeClient.getRenderLayer().ordinal();
+        BlockPos pos = origState.getValue(BlockCamoSlope.POS);
+        BlockColors colorEngine = FMLClientHandler.instance().getClient().getBlockColors();
+        WorldClient worldClient = FMLClientHandler.instance().getWorldClient();
         //Get the base color multiplier from the camo block can be set through state later
-        int i1 = FMLClientHandler.instance().getClient().getBlockColors().colorMultiplier(ConfigLib.CamoBlocks[0].getDefaultState(),FMLClientHandler.instance().getWorldClient(),origState.getValue(BlockCamoSlope.POS),MinecraftForgeClient.getRenderLayer().ordinal());
+        int i1 = colorEngine.colorMultiplier(origState,worldClient,pos, renderLayer);
 
         //If Side had a tint such as grass then multiply that with what we got so far
-        if (quad.hasTintIndex()) {
-            i1 = GetColorMulti(FMLClientHandler.instance().getClient().getBlockColors().colorMultiplier(camo,FMLClientHandler.instance().getWorldClient(),origState.getValue(BlockCamoSlope.POS),MinecraftForgeClient.getRenderLayer().ordinal())
-                    , i1
-            );
+        if (hasTint) {
+            i1 = GetColorMulti(colorEngine.colorMultiplier(camo,worldClient,pos,renderLayer), i1);
         }
 
         //and also multiply the color found in the vertex data
-        UVs[0] = GetColorMulti(UVs[0], i1);
-        UVs[3] = GetColorMulti(UVs[3], i1);
-        UVs[6] = GetColorMulti(UVs[6], i1);
-        UVs[9] = GetColorMulti(UVs[9], i1);
-        //Finally replace color, U and V info for each face in the camo model
-        return ReplaceTexture(origFace, UVs, quad.getSprite());
+        Cs[0] = GetColorMulti(Cs[0], i1);
+        Cs[1] = GetColorMulti(Cs[1], i1);
+        Cs[2] = GetColorMulti(Cs[2], i1);
+        Cs[3] = GetColorMulti(Cs[3], i1);
+
+        return Cs;
     }
 
     //This does the actual camouflage
-    private BakedQuad ReplaceTexture(BakedQuad origFace, int[] CUVs,TextureAtlasSprite sprite) {
-        int origInts[] = origFace.getVertexData();
-        //this returns an array in the following format
+    private int[] ReplaceTexture(int[] origInts,EnumFacing face , int[] UVs) {
 
-        switch(origFace.getFace()){
-            case DOWN: //possibly need to handle flipped texture
+        switch(face){
+            case DOWN:
             case UP:
-                CUVs = CalcUVs(new int[]{0,2},origInts,CUVs);
+                UVs = CalcUVs(
+                        Arrays.asList(
+                                Float.intBitsToFloat(origInts[0]),
+                                Float.intBitsToFloat(origInts[7]),
+                                Float.intBitsToFloat(origInts[14]),
+                                Float.intBitsToFloat(origInts[21])),
+                        Arrays.asList(
+                                Float.intBitsToFloat(origInts[2]),
+                                Float.intBitsToFloat(origInts[9]),
+                                Float.intBitsToFloat(origInts[16]),
+                                Float.intBitsToFloat(origInts[23])),
+                        UVs,
+                        face == EnumFacing.UP,
+                        true);
                 break;
             case NORTH:
             case SOUTH:
-                CUVs = CalcUVs(new int[]{0,1},origInts,CUVs);
+                UVs = CalcUVs(
+                        Arrays.asList(
+                                Float.intBitsToFloat(origInts[0]),
+                                Float.intBitsToFloat(origInts[7]),
+                                Float.intBitsToFloat(origInts[14]),
+                                Float.intBitsToFloat(origInts[21])),
+                        Arrays.asList(
+                                Float.intBitsToFloat(origInts[1]),
+                                Float.intBitsToFloat(origInts[8]),
+                                Float.intBitsToFloat(origInts[15]),
+                                Float.intBitsToFloat(origInts[22])),
+                        UVs,
+                        false,
+                        face == EnumFacing.SOUTH);
                 break;
             case WEST:
             case EAST:
-                CUVs = CalcUVs(new int[]{2,1},origInts,CUVs);
+                UVs = CalcUVs(
+                        Arrays.asList(
+                                Float.intBitsToFloat(origInts[2]),
+                                Float.intBitsToFloat(origInts[9]),
+                                Float.intBitsToFloat(origInts[16]),
+                                Float.intBitsToFloat(origInts[23])),
+                        Arrays.asList(
+                                Float.intBitsToFloat(origInts[1]),
+                                Float.intBitsToFloat(origInts[8]),
+                                Float.intBitsToFloat(origInts[15]),
+                                Float.intBitsToFloat(origInts[22])),
+                        UVs,
+                        false,
+                        face == EnumFacing.WEST);
                 break;
         }
+        return UVs;
 
-        //X,Y,Z,Color,U,V,?Light? for each point
-        int finalInts[] = {
-                origInts[0], origInts[1], origInts[2], CUVs[0], CUVs[1], CUVs[2],origInts[6],
-                origInts[7], origInts[8], origInts[9], CUVs[3], CUVs[4], CUVs[5],origInts[13],
-                origInts[14], origInts[15], origInts[16], CUVs[6], CUVs[7], CUVs[8],origInts[20],
-                origInts[21], origInts[22], origInts[23], CUVs[9], CUVs[10], CUVs[11],origInts[27]};
-        return new BakedQuad(finalInts, origFace.getTintIndex(), origFace.getFace(),sprite,true, DefaultVertexFormats.ITEM);
     }
 
     //modifies UVs to match
-    private int[] CalcUVs(int[] ints, int[] origInts, int[] CUVs) {
-        int maxU = Math.max(CUVs[1],Math.max(CUVs[4],Math.max(CUVs[7],CUVs[10])));
-        int deltaU = maxU - Math.min(CUVs[1],Math.min(CUVs[4],Math.min(CUVs[7],CUVs[10])));
-        int maxV = Math.max(CUVs[2],Math.max(CUVs[5],Math.max(CUVs[8],CUVs[11])));
-        int deltaV = maxV - Math.min(CUVs[2],Math.min(CUVs[5],Math.min(CUVs[8],CUVs[11])));
+    //origInts are used to figure out the cut off for the texture
+    private int[] CalcUVs(List<Float> mUs, List<Float> mVs, int[] UVs, boolean flipV, boolean flipU) {
+        List<Integer> Us = Arrays.asList(UVs[0], UVs[2], UVs[4], UVs[6]);
+        List<Integer> Vs = Arrays.asList(UVs[1], UVs[3], UVs[5], UVs[7]);
+        int maxU = Collections.max(Us);
+        int minU = Collections.min(Us);
+        int deltaU = maxU - minU;
+        int maxV = Collections.max(Vs);
+        int minV = Collections.min(Vs);
+        int deltaV = maxV - minV;
         //all the coords are float from 0 -1 stored as ints
+        //float shiftV = 1 -Collections.max(mVs); // Used to shift the textures down so that overlays like grass are in view
         //Use intBitsToFloat to get the correct decimal
-        for (int i=0;i<4;i++) {
-            CUVs[(i * 3) + 1] = (int)(maxU - (Float.intBitsToFloat(origInts[(7 * i) + ints[0]]) * deltaU));
-            CUVs[(i * 3) + 2] = (int)(maxV - (Float.intBitsToFloat(origInts[(7 * i) + ints[1]]) * deltaV));
+        for (int i = 0; i < 4; i++) {
+            if (flipU) {
+                UVs[(i * 2)] = (int) (minU + (mUs.get(i) * deltaU));
+            } else {
+                UVs[(i * 2)] = (int) (maxU - (mUs.get(i) * deltaU));
+            }
+            if (flipV) {
+                UVs[(i * 2) + 1] = (int) (minV + (mVs.get(i) * deltaV));
+            } else {
+                //UVs[(i * 2) + 1] = (int) (maxV - ((mVs.get(i) + shiftV) * deltaV)); //example usage of shiftV
+                UVs[(i * 2) + 1] = (int) (maxV - (mVs.get(i)  * deltaV));
+            }
         }
-
-        return CUVs;
+        return UVs;
     }
 
     private int GetColorMulti(int raw,int raw2) {
@@ -153,7 +222,9 @@ public class ModelBlockCamo implements IBakedModel {
     public List<BakedQuad> getQuads(IBlockState state, EnumFacing facing, long rand) {
         //get model of block if we didn't camo this will allow multiple models to be handled
         List<BakedQuad> origFaces = GetNonCamoModel((IExtendedBlockState) state).getQuads(state, null, rand);
+
         if (facing != null) {
+            List<BakedQuad> origSidedFaces = GetNonCamoModel((IExtendedBlockState) state).getQuads(state, facing, rand); //vanilla models actually do sided
             IBlockState camoState = ((IExtendedBlockState) state).getValue(BlockCamoSlope.BLOCKSTATES[facing.ordinal()]);
             if (camoState != null) {
                 List<BakedQuad> ret = new ArrayList<BakedQuad>();
@@ -161,11 +232,17 @@ public class ModelBlockCamo implements IBakedModel {
                     if (face.getFace() == facing)
                         ret.addAll(CamoSide((IExtendedBlockState) state, camoState, face));
                 }
+                for (BakedQuad face : origSidedFaces) {
+                        ret.addAll(CamoSide((IExtendedBlockState) state, camoState, face));
+                }
                 return ret;
             } else if (state.getBlock().canRenderInLayer(state, MinecraftForgeClient.getRenderLayer())) {
                 List<BakedQuad> ret = new ArrayList<BakedQuad>();
                 for (BakedQuad face : origFaces) {
                     if (face.getFace() == facing)
+                        ret.add(face);
+                }
+                for (BakedQuad face : origSidedFaces) {
                         ret.add(face);
                 }
                 return ret;
